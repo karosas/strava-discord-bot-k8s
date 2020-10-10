a60b5c4f1ce6d40b32b5ca9c67bbca50aecca4c3
Edgaras A <e.ausvicas@gmail.com>
2020-10-11 00:06:14 +0200

    Try hack #4

--- terraform/cluster/do_vms.tf
+++ terraform/cluster/do_vms.tf
@@ -14,7 +14,11 @@ resource "digitalocean_kubernetes_cluster" "cyber" {
     node_count = var.node_count
   }
 
-  provisioner "local-exec" {
-    command = "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod 0755 kubectl && ./kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/do/deploy.yaml"
+  provisioner "remote-exec" {
+    inline = [
+      "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
+      "chmod 0755 kubectl",
+      "./kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/do/deploy.yaml"
+    ]
   }
 }
